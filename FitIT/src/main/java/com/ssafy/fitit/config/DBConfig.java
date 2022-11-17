5a2734d4754ea76ed4d32b51d50ab3cf62dc8311
YunSuHee <suheeyun97@gmail.com>
2022-11-17 12:00:16 +0900

    config 추가

---
+++ FitIT/src/main/java/com/ssafy/fitit/config/DBConfig.java
@@ -0,0 +1,10 @@
+package com.ssafy.fitit.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan(basePackages = "com.ssafy.ssafit.model.dao")
+public class DBConfig {
+
+}
