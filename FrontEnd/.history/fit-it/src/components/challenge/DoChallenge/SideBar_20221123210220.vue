bf1d836fc01defb95a4ae24a491bbbef20f14f08
gimgyuyeon <8080claire@naver.com>
2022-11-23 21:59:32 +0900

    챌린지 참여하기 버튼 수정

---
+++ FrontEnd/.history/fit-it/src/components/challenge/DoChallenge/SideBar_20221123210220.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="title">
+        {{ challenge.challengeName }}
+      </div>
+      <li v-for="(mission, index) in indetailGetMissionList" :key="index">
+        <p @click="getMissionVideo(mission.missionNo)">
+          {{ video.videoTitle }}
+        </p>
+      </li>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "SideBar",
+  computed: {
+    ...mapState(["", "challenges", "challenge", "indetailGetMissionList"]),
+  },
+  created() {
+    this.$store.dispatch("getDetailChallenge", this.$route.params.challengeNo);
+    console.log(this.$route.params.challengeNo);
+    this.$store.dispatch(
+      "inDetailGetMissionList",
+      this.$route.params.challengeNo
+    );
+  },
+  methods: {
+    getMissionVideo(missionNo) {
+      this.$store.dispatch("getMissionVideo", missionNo);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.title {
+  font-size: 40px;
+  font-weight: 800;
+  color: indigo;
+  margin: 30px 10px 30px 10px;
+}
+p {
+  cursor: pointer;
+  margin: 30px 10px 30px 10px;
+  font-size: 20px;
+}
+p:hover {
+  color: indigo;
+  font-weight: 700;
+}
+</style>
---
+++ FrontEnd/.history/fit-it/src/views/HomeView_20221123203041.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div id="banner">
+      <div
+        id="carouselExampleCaptions"
+        class="carousel slide border-bottom border-1 pb-3"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="0"
+            class="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              id="carousel-item-img"
+              src="../"
+              class="d-block w-100 img-fluid"
+            />
+          </div>
+          <div class="carousel-item">
+            <img
+              id="carousel-item-img"
+              src="@/assets/img/banner2.png"
+              class="d-block w-100 img-fluid"
+            />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+    <div class="row mt-1">
+      <div class="row mt-3">
+        <div class="col-1"></div>
+        <div class="col">
+          <my-challenge></my-challenge>
+        </div>
+        <div class="col-1"></div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-1"></div>
+        <div class="col">
+          <open-challenge></open-challenge>
+        </div>
+        <div class="col-1"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import OpenChallenge from "@/components/challenge/OpenChallenge.vue";
+import MyChallenge from "@/components/challenge/MyChallenge.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    OpenChallenge,
+    MyChallenge,
+  },
+};
+</script>
+
+<style scoped></style>
